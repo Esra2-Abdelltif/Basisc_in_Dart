696736e4e870b54851c5f06c1f11915cc7fcc1a2
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-09-01 01:44:07 +0300

    define Question

---
+++ bin/Data Structures & Algorithms with Dart/Question/Question1.dart
@@ -0,0 +1,15 @@
+/// Question
+
+//Alice has some cards with numbers written on them.
+// She arranges the cards in decreasing order, and lays them out face down in a sequence on a table.
+// She challenges Bob to pick out the card containing a given number by turning over as few cards as possible.
+// Write a function to help Bob locate the card
+
+//***************
+
+//قبل ما احل المشكله لازم احدد شويه حاجات
+// 1-inPut ,outPut
+
+void main(List<String> arguments) {
+  print("Addtion(5, 10)");
+}
