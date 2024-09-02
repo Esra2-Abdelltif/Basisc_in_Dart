c2402ccd6cc39ef3198269d4748b12353e024ef9
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-09-02 23:03:17 +0300

    Recusrsion

--- bin/Data Structures & Algorithms with Dart/Recursion/Recursion.dart
+++ bin/Data Structures & Algorithms with Dart/Recursion/Recursion.dart
@@ -17,8 +17,9 @@ void countDown(int num){
 
 //Recusrsion Solution
 int factorial(int num){
+  //Fac 5=5*4*3*2*1
   if(num== 0){return 0;}
-  if (num == 1){return 1;}
+   if (num == 1){return 1;}
 
  return num * factorial(--num);
 
